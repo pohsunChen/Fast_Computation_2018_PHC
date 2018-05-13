eaac98271f5edfea6ea0ce3c859ff3b21bff0b73
Po-Hsun Chen <ricky987r@hotmail.com>
2018-05-13 15:34:44 +0800

    2018/05/13-single(final)

--- FFT_PHC_single.c
+++ FFT_PHC_single.c
@@ -6,11 +6,12 @@
 /// Function declaration
 void bit_reverse(double *x_re, double *x_im, double *y_re, double*y_im, int N);
 void butterfly(double *x_re, double *x_im, int N);
-int check_base(int *base, int N);
+void set_base(int *base, int N);
+int cal_id_b_max(int N);
 
 int main(){
     int i;
-    const int N = 180;
+    const int N = 16;
     double *x_re, *x_im, *y_re, *y_im;
 
     x_re = (double*) malloc(N*sizeof(double));
@@ -41,14 +42,18 @@ int main(){
 
 /// bit reverse for setting all input in place
 void bit_reverse(double *x_re, double *x_im, double *y_re, double*y_im, int N){
-	int base[3];
+	int id_b_max = cal_id_b_max(N);
+	int *base;
 	int id_b;   // index for base
-	int id_b_max = check_base(base, N); // set base and calculate id_b_max
-    int m = N/base[id_b_max];    // added number which is highest bit
+    int m;    // highest bit
     int p, q;       // p & q is the index that exchanges for each other
     int k;          // k is use to check digital (log_2 k + 1) if is 1
 
+    // set base
+	base = (int*) malloc((id_b_max+1)*sizeof(int));
+	set_base(base, N);
 
+    // p = 0 and N-1 are unchanged
 	y_re[0] = x_re[0];
     y_im[0] = x_im[0];
 	y_re[N-1] = x_re[N-1];
@@ -57,6 +62,7 @@ void bit_reverse(double *x_re, double *x_im, double *y_re, double*y_im, int N){
     // p is index before exchanging
     // from 0 to N-1 regularly
     // q is index after exchanging
+    m = N/base[id_b_max];    // added number which is highest bit
     q = m;  // first index of exchanged number
     // skip p = 0 & N-1 because they are exchanged by themselves
     for (p=1; p<N-1; p++){
@@ -89,24 +95,44 @@ void bit_reverse(double *x_re, double *x_im, double *y_re, double*y_im, int N){
 
 /// butterfly structure to recombine these input
 void butterfly(double *x_re, double *x_im, int N){
-	int base[3];
-	int id_b_max = check_base(base, N); // set base and calculate id_b_max
+	int id_b_max = cal_id_b_max(N);   // calculate id_b_max
+	int *base;
 	int id_b;	// base id: 0 for base2, 1 for base3, and 2 for base5
     int m;  // m is number in each input group
             // it's also the difference between z and u
     int k;  // index in RHS of butterfly (output) (from 0 to m)
             // the rest of m/2 is counterpart of the first m/2
-    int id[5];  // the index in LHS of butterfly (input1-5)
+    int *id;  // the index in LHS of butterfly (input1-5)
 
-	double w_k_re[5], w_k_im[5], w_N_re[5], w_N_im[5];
-    double w_but_re[3][5][5];
-    double w_but_im[3][5][5];
-    double temp, x_re_temp[5], x_im_temp[5];   // for temporary storage of number
+	double *w_k_re, *w_k_im, *w_N_re, *w_N_im;  // multiplier of counterpart
+    double ***w_but_re;     // multiplier on butterfly
+    double ***w_but_im;
+    double temp, *x_re_temp, *x_im_temp;   // for temporary storage of number
     int i, j, t;	// index for small loop
 
-	
-	
-	
+
+	// set base
+	base = (int*) malloc((id_b_max+1)*sizeof(int));
+	set_base(base, N);
+	// allocate memory
+	id = (int*) malloc(base[id_b_max]*sizeof(int));
+    w_k_re = (double*) malloc(base[id_b_max]*sizeof(double));
+    w_k_im = (double*) malloc(base[id_b_max]*sizeof(double));
+    w_N_re = (double*) malloc(base[id_b_max]*sizeof(double));
+    w_N_im = (double*) malloc(base[id_b_max]*sizeof(double));
+    x_re_temp = (double*) malloc(base[id_b_max]*sizeof(double));
+    x_im_temp = (double*) malloc(base[id_b_max]*sizeof(double));
+    w_but_re = (double***) malloc((id_b_max+1)*sizeof(double**));
+    w_but_im = (double***) malloc((id_b_max+1)*sizeof(double**));
+    for(i=0; i<(id_b_max+1); i++){
+        w_but_re[i] = (double**) malloc(base[id_b_max]*sizeof(double*));
+        w_but_im[i] = (double**) malloc(base[id_b_max]*sizeof(double*));
+        for (j=0; j<base[id_b_max]; j++){
+            w_but_re[i][j] = (double**) malloc(base[id_b_max]*sizeof(double));
+            w_but_im[i][j] = (double**) malloc(base[id_b_max]*sizeof(double));
+        }
+    }
+
     // initialization
     for (i=0; i<(id_b_max+1); i++){
     	for (j=0; j<base[id_b_max]; j++){
@@ -220,19 +246,39 @@ void butterfly(double *x_re, double *x_im, int N){
 }
 
 
-int check_base(int *base, int N){
-	int id_b_max = 0;
+int cal_id_b_max(int N){
+    int id_b_max = 0;
 	if (N%2 == 0){
-		base[id_b_max] = 2;
         id_b_max++;
 	}
 	if (N%3 == 0){
-		base[id_b_max] = 3;
 		id_b_max++;
 	}
 	if (N%5 == 0){
-		base[id_b_max] = 5;
+		id_b_max++;
+	}
+	if (N%7 == 0){
 		id_b_max++;
 	}
 	return --id_b_max;
+}
+
+void set_base(int *base, int N){
+    int i=0;
+	if (N%2 == 0){
+		base[i] = 2;
+		i++;
+	}
+	if (N%3 == 0){
+		base[i] = 3;
+		i++;
+	}
+	if (N%5 == 0){
+		base[i] = 5;
+		i++;
+	}
+	if (N%7 == 0){
+		base[i] = 7;
+		i++;
+	}
 }
